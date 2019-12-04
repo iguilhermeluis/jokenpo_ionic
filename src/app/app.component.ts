a3316b98899a2d5a66b5149e55d8dc1afc6a7dc6
Guilherme Luis <glm_m1@hotmail.com>
2019-12-04 14:02:39 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component, ViewChild } from "@angular/core";
+import { Nav, Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { HomePage } from "../pages/home/home";
+import { ListPage } from "../pages/list/list";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string; component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: "Home", component: HomePage },
+      { title: "List", component: ListPage }
     ];
-
   }
 
   initializeApp() {
@@ -32,6 +35,8 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(false);
+      this.statusBar.backgroundColorByHexString("#02073e");
       this.splashScreen.hide();
     });
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,87 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Vibration } from "@ionic-native/vibration";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  escolhaPlay: string;
+  escolhaComputador: string = "Escolha uma opção";
+  resultado: string = "Escolha uma opção";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private vibration: Vibration) {}
 
+  escolha(opcao) {
+    this.vibration.vibrate(500);
+    this.escolhaPlay = opcao;
+    this.jokepon();
   }
 
+  jokepon() {
+    let numAleatorio = Math.floor(Math.random() * 3);
+    let escolhaUsuario = this.escolhaPlay;
+    let escolhaComputador = "";
+
+    switch (numAleatorio) {
+      case 0:
+        escolhaComputador = "pedra";
+        break;
+      case 1:
+        escolhaComputador = "papel";
+        break;
+      case 2:
+        escolhaComputador = "tesoura";
+        break;
+      default:
+        escolhaComputador = "";
+    }
+
+    let resultado = "";
+
+    if (escolhaComputador === "pedra") {
+      if (escolhaUsuario === "pedra") {
+        resultado = "Empate";
+      }
+
+      if (escolhaUsuario === "papel") {
+        resultado = "Você ganhou";
+      }
+
+      if (escolhaUsuario === "tesoura") {
+        resultado = "Você perdeu";
+      }
+    }
+
+    if (escolhaComputador === "papel") {
+      if (escolhaUsuario === "papel") {
+        resultado = "Empate";
+      }
+
+      if (escolhaUsuario === "tesoura") {
+        resultado = "Você ganhou";
+      }
+
+      if (escolhaUsuario === "pedra") {
+        resultado = "Você perdeu";
+      }
+    }
+
+    if (escolhaComputador === "tesoura") {
+      if (escolhaUsuario === "tesoura") {
+        resultado = "Empate";
+      }
+
+      if (escolhaUsuario === "pedra") {
+        resultado = "Você ganhou";
+      }
+
+      if (escolhaUsuario === "papel") {
+        resultado = "Você perdeu";
+      }
+    }
+    this.escolhaComputador = escolhaComputador;
+    this.resultado = resultado;
+  }
 }
